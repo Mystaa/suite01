0d25d7a132d323da28c245b04167f373e9bea0cf
Antoine <antoine.thomire@gmail.com>
2017-03-13 10:35:04 +0100

    Ex10 fini + test

--- ex10.js
+++ ex10.js
@@ -4,12 +4,12 @@
 
     Un panier est un tableau de mots;
 
-    La fonction retourne un tableau d'objet contenant:
+    La fonction retourne un objet contenant:
         - comme clef le nom du produit rencontré
         - comme valeur le nombre de fois qu'il à été rencontré
 
     Important -> l'ordre n'a aucune importance
-*/
+    */
 
 /*      Test 1
     Appel à la fonction "shoppingList",
@@ -25,23 +25,66 @@
 
     et nous attendons comme résultat
 
-    [{
+    {
         "orange": 8,
         "kiwi": 4,
         "ananas": 3,
         "prune": 2,
         "banane": 2,
         "pamplemousse": 1
-    }]
+    }
 
-*/
+    */
 
-shoppingList([
-    ["orange", "orange", "kiwi", "ananas"],
-    ["kiwi", "ananas", "banane", "prune"],
-    ["orange", "orange", "orange", "orange"],
-    ["orange", "orange", "kiwi", "kiwi"],
-    ["prune", "banane", "pamplemousse", "ananas"]
-]);
+    console.log(shoppingList([
+        ["orange", "orange", "kiwi", "ananas"],
+        ["kiwi", "ananas", "banane", "prune"],
+        ["orange", "orange", "orange", "orange"],
+        ["orange", "orange", "kiwi", "kiwi"],
+        ["prune", "banane", "pamplemousse", "ananas"]
+        ]));
 
 //  écrire votre code sous ce commentaire
+function shoppingList(tableau) {
+    var nbk = 0;
+    var nba = 0;
+    var nbb = 0;
+    var nbp = 0;
+    var nbo = 0;
+    var nbpa = 0;
+    var panier = [];
+    
+    for (let i in tableau){
+
+        panier = tableau[i];
+
+        for (let j in panier){
+
+            if (panier[j] ==="orange"){
+                nbo++;
+            }
+            else if (panier[j] ==="ananas"){
+                nba++;
+            }
+            else if (panier[j] ==="kiwi"){
+                nbk++;
+            }
+            else if (panier[j] ==="prune"){
+                nbp++;
+            }
+            else if (panier[j] ==="pamplemousse"){
+                nbpa++;
+            }
+            else if (panier[j] ==="banane"){
+                nbb++;
+            }
+        }
+    }
+    return {
+        orange : nbo,
+        kiwi : nbk,
+        prune :nbp,
+        ananas :nba,
+        pamplemousse :nbpa,
+    }
+}
